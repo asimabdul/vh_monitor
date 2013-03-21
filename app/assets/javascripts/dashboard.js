66ab71c9b527e2d0cce4654e891a053adf7bd795
asimabdul <abdulasim@gmail.com>
2013-03-20 23:42:36 -0700

    blinking text and css changes

--- app/assets/javascripts/dashboard.js
+++ app/assets/javascripts/dashboard.js
@@ -1,7 +1,18 @@
 function getStatus() {
   $.get("/status", function(data){
-    $(".status-box").css("background-color", data.ci);
+    $(".status-box").css("color", data.ci);
+    if (data.ci == "blue") {
+      blink(".project-name");
+    }
   })
+}
+
+function blink(selector){
+  $(selector).fadeOut('slow', function(){
+    $(this).fadeIn('slow', function(){
+      blink(this);
+    });
+  });
 }
 
 $(document).ready(function(){
